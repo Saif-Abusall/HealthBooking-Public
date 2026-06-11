c72a11c46c35db4e8284d5738e8b1559e0c70477
Saif-Abusall <121250907+Saif-Abusall@users.noreply.github.com>
2026-06-11 22:46:22 +0300

    Update API endpoint for appointment booking

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -62,7 +62,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
+      fetch("https://o3mj0xti2i.execute-api.us-east-1.amazonaws.com/prod/appointments", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })
